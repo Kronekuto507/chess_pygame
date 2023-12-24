a47d9f691a01110903e797014cfdbf9ec2c0d266
Aarón Cigarruista <aaron18c1@gmail.com>
2023-12-24 15:47:07 -0500

    added board

---
+++ board/chess.py
@@ -0,0 +1,39 @@
+import pygame
+from pygame.locals import *
+
+
+WIDTH = 1000
+HEIGHT = 1000 
+class Board:
+    def __init__(self, screen):
+        
+        self.ROWS,self.COLS = 8,8
+        self.SIZE = WIDTH/self.COLS
+        self.next_color = pygame.Color(18,95,74) #Verde
+        self.starter_color = pygame.Color(250, 233, 169) #Crema
+        self.screen = screen
+    
+    def draw_cell(self,x,y, color):
+        pygame.draw.rect(self.screen, color, Rect(x,y,self.SIZE,self.SIZE))
+
+    def draw_board(self):
+        
+        for i in range(0,self.ROWS):
+
+            for j in range(0,self.COLS):
+
+                if i%2==0:
+                    if j%2==0:
+                        self.draw_cell(self.SIZE*j/2,self.SIZE*i/2, self.starter_color)
+                    else:
+                        self.draw_cell(self.SIZE*j/2,self.SIZE*i/2,self.next_color)
+                else:
+                    if j%2==0:
+                        self.draw_cell(self.SIZE*j/2,self.SIZE*i/2, self.next_color)
+                    else:
+                        self.draw_cell(self.SIZE*j/2,self.SIZE*i/2,self.starter_color)
+
+
+            
+            
+                
--- game.py
+++ game.py
@@ -1,21 +1,25 @@
 import pygame
 from pygame.locals import *
+from board.chess import *
+width, height = 1000,1000
 
 class Game:
     def __init__(self):
         pygame.init()
         self.flag = RESIZABLE
         self.is_running = True
-        self.size = (500, 500)
+        self.size = (width, height)
         self.screen = pygame.display.set_mode(self.size, self.flag)
     
     def run(self):
-    
+        chess_board = Board(self.screen)
         while self.is_running:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     self.is_running = False
             self.screen.fill(Color(225, 105,  255))
+            
+            chess_board.draw_board()
             pygame.display.update()
         pygame.quit()
 
