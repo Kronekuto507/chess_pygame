b9261c505062fc0e95ceefe998ea90a84b27f1d6
Aarón Cigarruista <aaron18c1@gmail.com>
2024-01-01 21:54:52 -0500

    cleaner for loops and added showing squares functionality

--- game.py
+++ game.py
@@ -11,12 +11,12 @@ class Game:
         self.is_running = True
         self.size = (WIDTH, HEIGHT)
         self.screen = pygame.display.set_mode(self.size)
-    
+    #Aniluz
     def run(self):
         chess_board = Board()
         chess_board.create_virtual_board(self.screen)
         while self.is_running:
-           
+          
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.is_running = False
@@ -25,32 +25,36 @@ class Game:
                     x,y = event.pos
 
                     #Fragmento de codigo que selecciona la pieza
-                    for row in range(ROWS):
-                        for col in range(COLS):
-                            if isinstance(chess_board.virtual_board[row][col],Piece):
+                    for row in chess_board.virtual_board:
+                        for piece in row:
+                            if isinstance(piece,Piece):
                                 if chess_board.selected_piece is None: #Se detecta si no existe ya un objeto seleccionado, de ser el caso, seleccionar directamente
-                                    chess_board.virtual_board[row][col].select_piece(x,y)
+                                    piece.select_piece(x,y)
                                 else: #Si ya hay un objeto, este 
-                                    selected_row = chess_board.selected_piece.get_row()
-                                    selected_col = chess_board.selected_piece.get_column()
+                                    selected_row = piece.get_row()
+                                    selected_col = piece.get_column()
 
                                     chess_board.virtual_board[selected_row][selected_col].deselect()
-                                    chess_board.virtual_board[row][col].select_piece(x,y)
+                                    piece.select_piece(x,y)
 
-
-                    
                     #asigna la pieca seleccionada en el fragmento de arriba al tablero
-                    for row in range(ROWS):
-                        for col in range(COLS):
-                            if isinstance(chess_board.virtual_board[row][col],Piece):
-                                print(chess_board.virtual_board[row][col].is_selected)
-                                if chess_board.virtual_board[row][col].is_selected:
-                                    chess_board.selected_piece = chess_board.virtual_board[row][col]
+                    for row in chess_board.virtual_board:
+                        for piece in row:
+                            if isinstance(piece,Piece):
+                                print(piece.is_selected)
+                                if piece.is_selected:
+                                    chess_board.selected_piece = piece
                     print(chess_board.selected_piece)
-                                
-            self.screen.fill(CREMA)
-            chess_board.draw_board(self.screen) 
 
+            #Dibujar tablero
+            self.screen.fill(CREMA)
+            chess_board.draw_board(self.screen)
+
+            #Si hay una pieza seleccionada, entonces esta muestra las celdas a las que puede ir  
+            for row in chess_board.virtual_board:
+                for piece in row:
+                    if isinstance(piece,Piece):
+                        piece.show_squares()
             pygame.display.update()
         pygame.quit()
 
