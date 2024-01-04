0a74e667537be42247017fe9ada436e454430405
Aarón Cigarruista <aaron18c1@gmail.com>
2024-01-03 23:17:19 -0500

    changes in the board_template file: better readability and changed the way images are drawn

--- board/board_template.py
+++ board/board_template.py
@@ -5,72 +5,57 @@ from .constants import VERDE,ROWS,COLS,SIZE
 
 
 class Board:
-    def __init__(self):
+    
+    def __init__(self, screen):
         #Plantilla en donde se dibujara el resto de cuadros
         #Diccionario que almacena las coordenadas de las superficie (Posiblemente innecesario, puede que lo elimine después)
         self.virtual_board = [] #Representacion virtual del tablero
         self.selected_piece = None
-        
+        self.screen = screen
+        b_starter_row = 0
+        self.b_array = [Rook('black',screen,b_starter_row,0),Knight('black',screen,b_starter_row,1)
+                   ,Bishop('black',screen,b_starter_row,2),Queen('black',screen,b_starter_row,3),King('black',screen,b_starter_row,4)
+                    ,Bishop('black',screen,b_starter_row,5),Knight('black',screen,b_starter_row,6),Rook('black',screen,b_starter_row,7)]
+        w_starter_row = 7
+        self.w_array = [Rook('white',screen,w_starter_row,0),Knight('white',screen,w_starter_row,1)
+                   ,Bishop('white',screen,w_starter_row,2),Queen('white',screen,w_starter_row,3),King('white',screen,w_starter_row,4)
+                    ,Bishop('white',screen,w_starter_row,5),Knight('white',screen,w_starter_row,6),Rook('white',screen,w_starter_row,7)]        
     
     def draw_cell(self,surface,color,x,y):
         pygame.draw.rect(surface,color,(x,y,SIZE,SIZE))
 
-    def draw_board(self,screen):
+    def draw_board(self):
         
         for row in range(0,ROWS):
             for col in range(row%2,ROWS,2):
                 coord_y,coord_x = SIZE*col,SIZE*row
-                self.draw_cell(screen,VERDE,coord_x,coord_y)
+                self.draw_cell(self.screen,VERDE,coord_x,coord_y)
 
-        b_starter_row = 0
-        b_array = [Rook('black',screen,b_starter_row,0),Knight('black',screen,b_starter_row,1)
-                   ,Bishop('black',screen,b_starter_row,2),Queen('black',screen,b_starter_row,3),King('black',screen,b_starter_row,4)
-                    ,Bishop('black',screen,b_starter_row,5),Knight('black',screen,b_starter_row,6),Rook('black',screen,b_starter_row,7)]
-        w_starter_row = 7
-        w_array = [Rook('white',screen,w_starter_row,0),Knight('white',screen,w_starter_row,1)
-                   ,Bishop('white',screen,w_starter_row,2),Queen('white',screen,w_starter_row,3),King('white',screen,w_starter_row,4)
-                    ,Bishop('white',screen,w_starter_row,5),Knight('white',screen,w_starter_row,6),Rook('white',screen,w_starter_row,7)]
-        
-        for row in range(ROWS):
-            b_array[row].set_image()
-            w_array[row].set_image()
-            for col in range(COLS):
-                    if row == 6:
-                        w_pawn = Pawn('white',screen,row,col)
-                        w_pawn.set_image()
-                    elif row == 1:
-                        b_pawn = Pawn('black', screen,row,col)
-                        b_pawn.set_image()
-        
-    
-
-        #Colocar piezas en el tablero
-
+        for row in self.virtual_board:
+            for piece in row:
+                if isinstance(piece,Piece):
+                    piece.set_image()
         
               
-    def create_virtual_board(self,screen):
-        b_starter_row = 0
-        b_array = [Rook('black',screen,b_starter_row,0),Knight('black',screen,b_starter_row,1)
-                   ,Bishop('black',screen,b_starter_row,2),Queen('black',screen,b_starter_row,3),King('black',screen,b_starter_row,4)
-                    ,Bishop('black',screen,b_starter_row,5),Knight('black',screen,b_starter_row,6),Rook('black',screen,b_starter_row,7)]
-        w_starter_row = 7
-        w_array = [Rook('white',screen,w_starter_row,0),Knight('white',screen,w_starter_row,1)
-                   ,Bishop('white',screen,w_starter_row,2),Queen('white',screen,w_starter_row,3),King('white',screen,w_starter_row,4)
-                    ,Bishop('white',screen,w_starter_row,5),Knight('white',screen,w_starter_row,6),Rook('white',screen,w_starter_row,7)]
+    def create_virtual_board(self):
         
         for row in range(ROWS):
             self.virtual_board.append([])
             for col in range(COLS):
                     if row == 6:
-                        w_pawn = Pawn('white',screen,row,col)
+                        w_pawn = Pawn('white',self.screen,row,col)
                         self.virtual_board[row].append(w_pawn)
+                        self.virtual_board[row][col].create_image()
                     elif row == 1:
-                        b_pawn = Pawn('black', screen,row,col)
+                        b_pawn = Pawn('black', self.screen,row,col)
                         self.virtual_board[row].append(b_pawn)
+                        self.virtual_board[row][col].create_image()
         
         for col in range (COLS):
-            self.virtual_board[0].append(b_array[col])
-            self.virtual_board[7].append(w_array[col])
+            self.virtual_board[0].append(self.b_array[col])
+            self.virtual_board[7].append(self.w_array[col])
+            self.virtual_board[0][col].create_image()
+            self.virtual_board[7][col].create_image()
         
         for row in range(ROWS):
             for col in range(COLS):
