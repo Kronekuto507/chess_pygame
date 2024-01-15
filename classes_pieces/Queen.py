6409a546fa6bfe549ea079c1d14d3e81bef15e95
Aarón Cigarruista <aaron18c1@gmail.com>
2024-01-15 12:58:29 -0500

    created the queen class in the file so it generates moves

--- classes_pieces/Queen.py
+++ classes_pieces/Queen.py
@@ -3,3 +3,19 @@ from pygame.locals import *
 from classes_pieces.pieces import Piece
 from classes_pieces.Bishop import Bishop
 from classes_pieces.Rook import Rook
+from classes_pieces.King import King
+
+class Queen(Piece):
+    def __init__(self, color, surface, row, col):
+        super().__init__(color, surface, row, col)
+        self.name = 'queen'
+        self.rook_instance = Rook(color,surface,row,col)
+        self.bishop_instance = Bishop(color,surface,row,col)
+        self.king_instance = King(color,surface,row,col)
+        '''Chequear todo eso de arriba después'''
+    def generate_moves(self,board):
+        moves = []
+        moves.extend(self.rook_instance.generate_moves(board))
+        moves.extend(self.bishop_instance.generate_moves(board))
+        moves.extend(self.king_instance.generate_moves(board))
+        return moves
