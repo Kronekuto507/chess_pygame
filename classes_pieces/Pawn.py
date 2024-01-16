2194fca5cae8eeab3c16b38f89f5dc16152a8d70
Aarón Cigarruista <aaron18c1@gmail.com>
2024-01-15 22:12:18 -0500

    boilerplate for pawn piece

---
+++ classes_pieces/Pawn.py
@@ -0,0 +1,8 @@
+import pygame
+from pygame.locals import *
+from classes_pieces.pieces import Piece
+from board.board_template import Board
+class Pawn(Piece):
+    def __init__(self, color, surface, row, col):
+        super().__init__(color, surface, row, col)
+        self.name = 'pawn'
