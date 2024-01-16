27dadf5c2beaf3d361baf2e74205b96776322aaa
Aarón Cigarruista <aaron18c1@gmail.com>
2024-01-16 16:39:21 -0500

    cleaned the code, it's more redable now

--- classes_pieces/Rook.py
+++ classes_pieces/Rook.py
@@ -11,31 +11,24 @@ class Rook(Piece):
 
     def generate_moves(self,board):
 
-        offsett = (COLS * -1) + self.col
-        later_offsett = (COLS * -1)
-
-        east = []
-
-        for col in range(self.col, COLS + 1):
-            if isinstance(board[self.row][col],int):
-                east.append((self.row,col))
-            else:
-                break
-
-        west = []
-
-        for col in range(offsett, later_offsett + 1):
-            if isinstance(board[self.row][col], int):
-                west.append((self.row, COLS + col))
         
-        vertical = []
-
-        for col in range(COLS):
-            for row in range(ROWS):
-                if col == self.col:
-                    if isinstance(board[row][col],int):
-                        vertical.append(row,col)
-                        
-        moves = east + west + vertical
+        east = self.calc_rook_moves(self.col,COLS,1,board)
+        west = self.calc_rook_moves(self.col,-1,-1,board)
+        north = self.calc_rook_moves(self.row,-1,-1,board,exchange_rowNcol=True)
+        south = self.calc_rook_moves(self.row,ROWS,1,board,exchange_rowNcol=True)
+ 
+        moves = east + west + north + south
 
         return moves
+    
+    def calc_rook_moves(self,start,end,step, board,exchange_rowNcol = False):
+        array_move = []
+        if exchange_rowNcol:
+            for row in range(start,end,step):
+                if isinstance(board[self.col][row],int):
+                    array_move.append((row,self.col))
+        else:
+            for col in range(start,end,step):
+                if isinstance(board[self.row][col],int):
+                    array_move.append((self.row,col))
+        return array_move
