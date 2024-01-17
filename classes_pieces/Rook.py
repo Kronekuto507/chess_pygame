b3864e1bf52677b9e6b9ca698e7758070cdade4b
Aarón Cigarruista <aaron18c1@gmail.com>
2024-01-16 22:38:40 -0500

    functionality of the rook's movement completed, I guess?

--- classes_pieces/Rook.py
+++ classes_pieces/Rook.py
@@ -11,7 +11,6 @@ class Rook(Piece):
 
     def generate_moves(self,board):
 
-        
         east = self.calc_rook_moves(self.col,COLS,1,board)
         west = self.calc_rook_moves(self.col,-1,-1,board)
         north = self.calc_rook_moves(self.row,-1,-1,board,exchange_rowNcol=True)
@@ -21,14 +20,30 @@ class Rook(Piece):
 
         return moves
     
-    def calc_rook_moves(self,start,end,step, board,exchange_rowNcol = False):
+    def calc_rook_moves(self,start,end,step,board,exchange_rowNcol = False):
         array_move = []
         if exchange_rowNcol:
             for row in range(start,end,step):
-                if isinstance(board[self.col][row],int):
-                    array_move.append((row,self.col))
+                if isinstance(board.virtual_board[row][self.col],int):
+                    array_move.append((row,self.col)) #Que carajo era ese error lols
+                else:
+                    if row != self.row:
+                        if isinstance(board.virtual_board[row][self.col],Piece):
+                            if board.is_ally_piece(self,board.virtual_board[row][self.col]):
+                                break
+                            else:
+                                array_move.append((row,self.col))
+                                break
         else:
             for col in range(start,end,step):
-                if isinstance(board[self.row][col],int):
+                if isinstance(board.virtual_board[self.row][col],int):
                     array_move.append((self.row,col))
+                else:
+                    if col != self.col:
+                        if isinstance(board.virtual_board[self.row][col],Piece):
+                            if board.is_ally_piece(self,board.virtual_board[self.row][col]):
+                                break
+                            else:
+                                array_move.append((self.row,col))
+                                break
         return array_move
