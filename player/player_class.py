2622387f33f5d4b4ce2db94c4271019b42db1497
Aarón Cigarruista <aaron18c1@gmail.com>
2024-01-18 15:46:28 -0500

    did some adjustments of the class

--- player/player_class.py
+++ player/player_class.py
@@ -1,15 +1,11 @@
 
-black_pieces = []
-white_pieces = []
 class Player:
-    def __init__(self, player_color, captured_pieces):
+    def __init__(self, player_color):
         self.player_color = player_color
         self.is_checkmated = False
         self.is_checked = False
-        self.captured_pieces = captured_pieces
+        self.captured_pieces = []
         self.player_turn = True
-    def move_piece(self):
-        pass
-    def set_player_turn(self, color):
-        pass
+    def add_caputred(self, piece):
+        self.captured_pieces.append(piece)
 
