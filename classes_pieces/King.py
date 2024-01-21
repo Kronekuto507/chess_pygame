3c7f4fb68513142549aef0c6386b53ec83afade5
Aarón Cigarruista <aaron18c1@gmail.com>
2024-01-21 12:36:23 -0500

    functionality of the castle method done almost correct,  but there's an index error bug. Will update soon

--- classes_pieces/King.py
+++ classes_pieces/King.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from classes_pieces.Piece import Piece
 from board.constants import *
+from classes_pieces.Rook import Rook
 
 class King(Piece):
     def __init__(self,color,surface,row,col):
@@ -9,8 +10,9 @@ class King(Piece):
         self.name = 'king'
         self.has_moved = False
         self.is_checked = False
-        self.castling_queen_side = (self.row,self.col - 2)
-        self.castling_king_side = (self.row,self.col + 2)
+        self.queen_side_pos = self.col - 2
+        self.king_side_pos = self.col + 2
+
     def generate_moves(self, board):
         starter_row = self.row - 1
         later_row = self.row + 2
@@ -20,12 +22,31 @@ class King(Piece):
         for row in range(starter_row,later_row):
             for col in range(starter_col,later_col):
                 if row<= 7 and row >= 0 and col <= 7 and col>= 0:
-                    if isinstance(board.virtual_board[row][col],int):
+                    if isinstance(board.virtual_board[row][col],int) or not board.is_ally_piece(self,board.virtual_board[row][col]):
                         moves.append((row,col))
-                        if self.has_moved == False:
-                            moves.append((self.row,self.col + 2 ))
-                            moves.append((self.row,self.col - 2))
+
+        if not self.has_moved:
+            king_side_pos = self.col + 2
+            queen_side_pos = self.col - 2
+            rooks = self.get_rooks(board)
+            if not rooks[1].has_moved and (isinstance(board.virtual_board[self.row][self.col + 1],int) and 
+                isinstance(board.virtual_board[self.row][self.col + 2],int)):
+                moves.append((self.row,king_side_pos))
+            if not rooks[0].has_moved and (isinstance(board.virtual_board[self.row][self.col - 1],int) and
+                isinstance(board.virtual_board[self.row][self.col - 2],int) and
+                isinstance(board.virtual_board[self.row][self.col - 3], int)):
+                moves.append((self.row,queen_side_pos))
+                     
         return moves
+    
+    def get_rooks(self,board):
+        rook_array = []
+        for col in range(0,COLS):
+            if isinstance(board.virtual_board[self.row][col],Rook):
+                rook_array.append(board.virtual_board[self.row][col])
+        return rook_array
+
+        
     
         
 
