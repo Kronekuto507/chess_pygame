f4ee4d4a96f1b1c36d98218a409f56ef7d907313
Aarón Cigarruista <aaron18c1@gmail.com>
2024-01-28 20:17:44 -0500

    moved valid moves for the king to the board_template class

--- classes_pieces/King.py
+++ classes_pieces/King.py
@@ -37,7 +37,7 @@ class King(Piece):
                 isinstance(board.virtual_board[self.row][self.col - 3], int)):
                 moves.append((self.row,queen_side_pos))
         
-        valid_moves = self.get_valid_moves(moves,board.get_enemy_pieces())
+        valid_moves = moves
         return valid_moves
     
     def get_rooks(self,board):
@@ -46,15 +46,6 @@ class King(Piece):
             if isinstance(board.virtual_board[self.row][col],Rook):
                 rook_array.append(board.virtual_board[self.row][col])
         return rook_array
-    
-    def get_valid_moves(self,moves,enemy_pieces): #HAY UN BUG CON EL PEON
-        valid_moves = moves
-        for piece in enemy_pieces:
-            for move in moves:
-                for piece_move in piece.moves:
-                    if move == piece_move and piece.color != self.color:
-                        valid_moves.remove(move)
-        return valid_moves
 
         
     
