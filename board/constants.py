588c824706a00b2cc5199f29bfd9cfedeba6fedf
Aarón Cigarruista <aaron18c1@gmail.com>
2024-02-22 22:16:44 -0500

    added constant screen variable

--- board/constants.py
+++ board/constants.py
@@ -1,8 +1,12 @@
 import pygame
 from pygame import locals
 
+
 WIDTH = 600
 HEIGHT = 600
+
+SCREEN_CENTER = WIDTH/2
+
 VERDE = pygame.Color(118,150,86) #Verde
 CREMA = pygame.Color(238,238,210) #Crema
 ROWS,COLS = 8,8
@@ -11,5 +15,5 @@ DIM_GREY= (189,195,199,0.7)
 TRANSPARENT_GREEN = (0,255,0)
 TRANSPARENT_BLUE = (30,144,255)
 TRANSPARENT_RED= (255,0,0)
-
 SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+
