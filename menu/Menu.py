97f43c73ea114b061eb8a48515ed8e2a2e6ec46f
Aarón Cigarruista <aaron18c1@gmail.com>
2024-03-03 19:43:08 -0500

    changed leaderboard to statistics

--- menu/Menu.py
+++ menu/Menu.py
@@ -12,12 +12,12 @@ CREATE_USER = Button(x_pos=PLACE, y_pos=225, text="Create User",
                     main_surface=SCREEN, base_color="white",hovering_color="green")
 PLAY = Button(x_pos=PLACE,y_pos=275,text="Play",
                 main_surface=SCREEN,base_color="white",hovering_color="black")
-LEADERBOARD = Button(x_pos=PLACE,y_pos=325, text= "Leaderboard",
+STATISTICS = Button(x_pos=PLACE,y_pos=325, text= "Statistics",
                     main_surface= SCREEN, base_color= "white", hovering_color = "green")
 QUIT = Button(x_pos=PLACE,y_pos=400,text="Quit",main_surface=SCREEN,base_color="white",hovering_color="green")
 
 FUNCTION_LIST = [create_user,play,leader_board,quit]
-BUTTON_LIST = [CREATE_USER,PLAY,LEADERBOARD,QUIT]
+BUTTON_LIST = [CREATE_USER,PLAY,STATISTICS,QUIT]
 DICTIONARY_BUTTONS = {button:foo for button,foo in zip(BUTTON_LIST,FUNCTION_LIST)}
 
 class Menu():
@@ -26,10 +26,10 @@ class Menu():
         
         while True:
             SCREEN.blit(BG_1,(0,0))
-            prov = pygame.font.SysFont("cambria",150)
+            prov = pygame.font.SysFont("Arial",150)
             MOUSE_POS_MENU = pygame.mouse.get_pos()
-            MENU_TEXT = prov.render("CHESS",True, "white")
-            MENU_RECT = MENU_TEXT.get_rect(center = (MENU_TEXT.get_width() - 200,100))
+            MENU_TEXT = prov.render("Chess",True, "white")
+            MENU_RECT = MENU_TEXT.get_rect(center = (MENU_TEXT.get_width() - 150,100))
             
             
             SCREEN.blit(MENU_TEXT,MENU_RECT)
@@ -47,8 +47,6 @@ class Menu():
                     for button,function in DICTIONARY_BUTTONS.items():
                         if button.check_input(MOUSE_POS_MENU):
                             button.on_click_behaviour(function)
-            
-
 
             pygame.display.update()
 
